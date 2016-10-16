0f368574508131a6caa6a1f5aca7ea6d1097afd9
lsnyder <luke.snyder@caa.com>
2016-10-16 15:44:31 -0700

    WIP

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My First Angular App</h1>
+    <h2>This is an h2 tag</h2>
+  `
+})
+
+export class AppComponent { }
